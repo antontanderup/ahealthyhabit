4507a412e621e57430e13f91d8cbd125d2cc3a1e
Anton Tanderup Adamsen <antontandrup@gmail.com>
2021-05-27 18:59:25 +0200

    Copy changes and prettier

--- src/components/Habbit/index.tsx
+++ src/components/Habbit/index.tsx
@@ -1,7 +1,7 @@
-import { isToday } from "date-fns";
-import React, { useMemo, useState } from "react";
-import { View, StyleSheet } from "react-native";
-import i18n from "i18n-js";
+import {isToday} from 'date-fns';
+import React, {useMemo, useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import i18n from 'i18n-js';
 import {
   Avatar,
   Card,
@@ -9,31 +9,31 @@ import {
   Chip,
   IconButton,
   ProgressBar,
-  useTheme,
-} from "react-native-paper";
-import { Habbit, markTodayDone, markTodayUndone, store } from "../../store";
-import { getStreaks } from "../../utils/calculateStreaks";
-import EditHabbit from "../EditHabbit";
+} from 'react-native-paper';
+import {Habbit, markTodayDone, markTodayUndone, store} from '../../store';
+import {getStreaks} from '../../utils/calculateStreaks';
+import EditHabbit from '../EditHabbit';
 
-export default ({ habbit }: { habbit: Habbit }) => {
-  const { colors } = useTheme();
-
+export default ({habbit}: {habbit: Habbit}) => {
   // Process streaks
-  const streaksDateArray = useMemo(() => getStreaks(habbit.recordedDates), [
-    habbit.recordedDates,
-  ]);
+  const streaksDateArray = useMemo(
+    () => getStreaks(habbit.recordedDates),
+    [habbit.recordedDates],
+  );
 
   // Helper consts
   const latestStreak = streaksDateArray[0];
   const lastDoneDate = latestStreak && latestStreak.dates[0];
   const doneToday = isToday(lastDoneDate);
-  const currentStreak = latestStreak?.dates?.length || 0;
+  const currentStreak = latestStreak?.isCurrentStreak
+    ? latestStreak?.dates?.length
+    : 0;
   const longestStreak: number = useMemo(() => {
-    const streaklengths = streaksDateArray.map((streak) => streak.dates.length);
+    const streaklengths = streaksDateArray.map(streak => streak.dates.length);
     return streaklengths.length > 0
       ? streaklengths.reduce((a, b) => Math.max(a, b))
       : 0;
-  }, [currentStreak]);
+  }, [streaksDateArray]);
 
   // Habbit editor state
   const [editorOpen, setEditorOpen] = useState(false);
@@ -50,36 +50,37 @@ export default ({ habbit }: { habbit: Habbit }) => {
           !isDisabled && !goalReached ? (
             <ProgressBar progress={progress} />
           ) : goalReached && !isDisabled ? (
-            <Avatar.Icon icon="star" size={28}/>
+            <Avatar.Icon icon="star" size={28} />
           ) : null
         }
         key={`goal${goal}`}
-        style={styles.chip}
-      >
-        {i18n.t("Days.count", { count: goal })}
+        style={styles.chip}>
+        {i18n.t('Days.count', {count: goal})}
       </Chip>
     );
   };
 
   return (
-    <Card style={{ marginHorizontal: 10, marginTop: 10, paddingBottom: 12 }}>
+    <Card style={{marginHorizontal: 10, marginTop: 10, paddingBottom: 12}}>
       <Card.Title
         title={habbit.name}
-        subtitle={i18n.t(doneToday ? "habbit.currentTodayDone.count" : "habbit.current.count", { count: currentStreak })}
+        subtitle={i18n.t(
+          doneToday ? 'habbit.currentTodayDone.count' : 'habbit.current.count',
+          {count: currentStreak},
+        )}
         right={() => (
           <View
             style={{
-              flexDirection: "row",
-              alignItems: "center",
+              flexDirection: 'row',
+              alignItems: 'center',
               marginRight: 10,
-            }}
-          >
+            }}>
             <IconButton
               icon="dots-vertical"
               onPress={() => setEditorOpen(true)}
             />
             <Checkbox
-              status={doneToday ? "checked" : "unchecked"}
+              status={doneToday ? 'checked' : 'unchecked'}
               onPress={() => {
                 if (doneToday) {
                   store.dispatch(markTodayUndone(habbit.id));
@@ -91,10 +92,10 @@ export default ({ habbit }: { habbit: Habbit }) => {
           </View>
         )}
       />
-      <Card.Content style={{ flexDirection: "row", flexWrap: "wrap" }}>
-        {longestStreak > 0 && (
+      <Card.Content style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+        {streaksDateArray.length > 1 && longestStreak > 0 && (
           <Chip style={styles.chip}>
-            {i18n.t("habbit.longest.count", { count: longestStreak })}
+            {i18n.t('habbit.longest.count', {count: longestStreak})}
           </Chip>
         )}
         {habbit?.goals?.map(goalChip)}
@@ -109,5 +110,5 @@ export default ({ habbit }: { habbit: Habbit }) => {
 };
 
 const styles = StyleSheet.create({
-  chip: { marginRight: 8, marginBottom: 10 },
+  chip: {marginRight: 8, marginBottom: 10},
 });
--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -1,91 +1,88 @@
-import i18n, { translations } from 'i18n-js';
+import i18n from 'i18n-js';
 // Set the key-value pairs for the different languages you want to support.
 i18n.translations = {
-    en: {
-        appName: 'A Healthy Habit',
-        Edit: 'Edit',
-        'Habbit name': 'Habbit name',
-        Done: 'Done',
-        Delete: 'Delete',
-        Save: 'Save',
-        "Add habbit": "Add habbit",
-        Goals: 'Goals',
-        Days: {
-            count: {
-                one: "1 day",
-                other: "{{count}} days",
-                zero: "0 days"
-            }
-        },
-        habbit: {
-            current: {
-                count: {
-                    one: "1 day. Lets keep it going!",
-                    other: "You are on day {{count}}. Keep it up!",
-                    zero: 'Lets go!'
-                }
-            },
-            currentTodayDone: {
-                count: {
-                    one: "1 day. See you tomorrow!",
-                    other: "Day {{count}}. Good job!",
-                    zero: 'Lets go!'
-                }
-            },
-            longest: {
-                count: {
-                    one: "Best: 1 day",
-                    other: "Best: {{count}} days",
-                    zero: ""
-                }
-                
-            }
-
-        }
+  en: {
+    appName: 'A Healthy Habit',
+    Edit: 'Edit',
+    'Habbit name': 'Habbit name',
+    Done: 'Done',
+    Delete: 'Delete',
+    Save: 'Save',
+    'Add habbit': 'Add habbit',
+    Goals: 'Goals',
+    Days: {
+      count: {
+        one: '1 day',
+        other: '{{count}} days',
+        zero: '0 days',
+      },
     },
-    da: {
-        appName: 'En God Vane',
-        Edit: 'Rediger',
-        'Habbit name': "Navn på vane",
-        Done: 'Færdig',
-        Delete: 'Slet',
-        Save: 'Gem',
-        "Add habbit": "Tilføj vane",
-        Goals: 'Mål',
-        Days: {
-            count: {
-                one: "1 dag",
-                other: "{{count}} dage",
-                zero: "0 dage"
-            }
+    habbit: {
+      current: {
+        count: {
+          one: '1 day. Lets make this a habbit!',
+          other: '{{count}} days. Keep it up!',
+          zero: 'Lets go!',
         },
-        habbit: {
-            current: {
-                count: {
-                    one: "1 dag. Go go go!",
-                    other: "Du er på dag {{count}}. Hold gejsten oppe!",
-                    zero: 'Sæt igang!'
-                }
-            },
-            currentTodayDone: {
-                count: {
-                    one: "1 day. Ses i morgen!",
-                    other: "Dag {{count}}. Godt klaret!",
-                    zero: 'Sæt igang!'
-                }
-            },
-            longest: {
-                count: {
-                    one: "Bedste: 1 dag",
-                    other: "Bedste: {{count}} dage",
-                    zero: ""
-                }
-                
-            }
-        }
+      },
+      currentTodayDone: {
+        count: {
+          one: 'Day {{count}}. It starts here.',
+          other: 'Day {{count}}. Good job!',
+          zero: 'Lets go!',
+        },
+      },
+      longest: {
+        count: {
+          one: 'Best: 1 day',
+          other: 'Best: {{count}} days',
+          zero: '',
+        },
+      },
     },
+  },
+  da: {
+    appName: 'En God Vane',
+    Edit: 'Rediger',
+    'Habbit name': 'Navn på vane',
+    Done: 'Færdig',
+    Delete: 'Slet',
+    Save: 'Gem',
+    'Add habbit': 'Tilføj vane',
+    Goals: 'Mål',
+    Days: {
+      count: {
+        one: '1 dag',
+        other: '{{count}} dage',
+        zero: '0 dage',
+      },
+    },
+    habbit: {
+      current: {
+        count: {
+          one: '1 dag. Det starter her.',
+          other: '{{count}} dage. Hold gejsten oppe!',
+          zero: 'Sæt igang!',
+        },
+      },
+      currentTodayDone: {
+        count: {
+          one: 'Dag {{count}}. Ses i morgen!',
+          other: 'Dag {{count}}. Godt klaret!',
+          zero: 'Sæt igang!',
+        },
+      },
+      longest: {
+        count: {
+          one: 'Bedste: 1 dag',
+          other: 'Bedste: {{count}} dage',
+          zero: '',
+        },
+      },
+    },
+  },
 };
 // Set the locale once at the beginning of your app.
-i18n.locale = "eng";
+i18n.locale = 'eng';
 
 i18n.fallbacks = true;
