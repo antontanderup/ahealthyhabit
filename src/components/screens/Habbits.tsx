6195a5e537e38317c55e1831fb92a07cca2ef447
Anton Tanderup Adamsen <antontandrup@gmail.com>
2021-05-27 18:57:53 +0200

    Fix state not updated when it is the next day

--- src/components/screens/Habbits.tsx
+++ src/components/screens/Habbits.tsx
@@ -1,53 +1,78 @@
-import React, { useState } from 'react';
-import { View, FlatList } from 'react-native';
-import { Appbar, FAB, useTheme } from 'react-native-paper';
-import { connect } from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import {View, FlatList, AppState, AppStateStatus} from 'react-native';
+import {Appbar, FAB, useTheme} from 'react-native-paper';
+import {connect} from 'react-redux';
 import i18n from 'i18n-js';
-import { Habbit as HabbitType, RootState } from '../../store';
+import {Habbit as HabbitType, RootState} from '../../store';
 import Habbit from '../Habbit';
 import EditHabbit from '../EditHabbit';
+import {getDate} from 'date-fns';
 
+const Habbits = ({habbits}: {habbits: RootState['habbits']}) => {
+  const {colors} = useTheme();
 
-const Habbits = ({ habbits }: { habbits: RootState['habbits'] }) => {
+  // current date is used in keyextractor so we refresh the components
+  // when a fresh day arives :)
+  const [currentDate, setCurrentDate] = useState(getDate(new Date()));
 
-    const { colors } = useTheme();
+  useEffect(() => {
+    AppState.addEventListener('change', _handleAppStateChange);
 
-    const [showAddHabbit, setShowAddHabbit] = useState(false);
+    return () => {
+      AppState.removeEventListener('change', _handleAppStateChange);
+    };
+  }, []);
 
-    const renderHabbit = ({ item }: { item: HabbitType }) => {
-        return <Habbit habbit={item} />
+  const _handleAppStateChange = (nextAppState: AppStateStatus) => {
+    if (nextAppState === 'active') {
+      setCurrentDate(getDate(new Date()));
     }
+  };
 
-    const flatListData = Object.values(habbits);
+  const [showAddHabbit, setShowAddHabbit] = useState(false);
 
-    return (
-        <View style={{ width: '100%', height: '100%', backgroundColor: colors.background }}>
-            <Appbar.Header>
-                <Appbar.Content title={i18n.t("appName")} />
-            </Appbar.Header>
-            <FlatList
-                data={flatListData}
-                renderItem={renderHabbit}
-                ListFooterComponent={<View style={{height: 100}}/>}
-            />
-            { !showAddHabbit ? (
-                <FAB
-                    style={{ position: 'absolute', bottom: 0, right: 0, margin: 16 }}
-                    icon="plus"
-                    onPress={() => setShowAddHabbit(true)}
-                />
-            ) : (
-                <EditHabbit isOpen={showAddHabbit} onClose={() => setShowAddHabbit(false)} />
-            )}
+  const renderHabbit = ({item}: {item: HabbitType}) => {
+    return <Habbit habbit={item} />;
+  };
 
-        </View>
-    );
+  const flatListData = Object.values(habbits);
+
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: colors.background,
+      }}>
+      <Appbar.Header>
+        <Appbar.Content title={i18n.t('appName')} />
+      </Appbar.Header>
+      <FlatList
+        data={flatListData}
+        renderItem={renderHabbit}
+        keyExtractor={item => item.id + currentDate}
+        ListFooterComponent={<View style={{height: 100}} />}
+      />
+      {!showAddHabbit ? (
+        <FAB
+          style={{position: 'absolute', bottom: 0, right: 0, margin: 16}}
+          icon="plus"
+          onPress={() => setShowAddHabbit(true)}
+        />
+      ) : (
+        <EditHabbit
+          isOpen={showAddHabbit}
+          onClose={() => setShowAddHabbit(false)}
+        />
+      )}
+    </View>
+  );
 };
 
 const mapStateToProps = (state: RootState) => {
-    return {
-        habbits: state.habbits
-    }
-}
+  return {
+    habbits: state.habbits,
+  };
+};
 
 export default connect(mapStateToProps)(Habbits);
