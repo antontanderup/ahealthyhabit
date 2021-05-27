df9a7dabeafd1f311bb36f3dbd86366bbddc2de2
Anton Tanderup Adamsen <antontandrup@gmail.com>
2021-05-27 18:56:39 +0200

    Fix currentStreak not always applied

--- src/utils/calculateStreaks.test.ts
+++ src/utils/calculateStreaks.test.ts
@@ -1,78 +1,87 @@
-import { getStreaks } from './calculateStreaks';
+import {getStreaks} from './calculateStreaks';
 
-describe("getStreaks", () => {
+describe('getStreaks', () => {
+  it('returns one streak with two entries', () => {
+    const testObject = {
+      name: 'Develop Habbit app',
+      id: 'rEoEKuOAHKFsQMYM04S_G',
+      recordedDates: ['Sat Apr 03 2021', 'Fri Apr 02 2021'],
+    };
+    const outputStreaks = getStreaks(testObject.recordedDates);
+    const expectedOutput = [
+      {
+        dates: testObject.recordedDates.map(dateString =>
+          Date.parse(dateString),
+        ),
+      },
+    ];
+    expect(outputStreaks).toEqual(expectedOutput);
+  });
 
-    it('returns one streak with two entries', () => {
-        const testObject = {
-            "name": "Develop Habbit app",
-            "id": "rEoEKuOAHKFsQMYM04S_G",
-            "recordedDates": [
-                "Sat Apr 03 2021",
-                "Fri Apr 02 2021"
-            ]
-        };
-        const outputStreaks = getStreaks(testObject.recordedDates);
-        const expectedOutput = [
-            {
-                dates: testObject.recordedDates.map(dateString => Date.parse(dateString))
-            }
-        ]
-        expect(outputStreaks).toEqual(expectedOutput);
-    });
+  it('returns single streak', () => {
+    const testObject = {
+      name: 'Develop Habbit app',
+      id: 'rEoEKuOAHKFsQMYM04S_G',
+      recordedDates: ['Sat Apr 03 2021'],
+    };
+    const outputStreaks = getStreaks(testObject.recordedDates);
+    const expectedOutput = [
+      {
+        dates: testObject.recordedDates.map(dateString =>
+          Date.parse(dateString),
+        ),
+      },
+    ];
+    expect(outputStreaks).toEqual(expectedOutput);
+  });
 
-    it('returns single streak', () => {
-        const testObject = {
-            "name": "Develop Habbit app",
-            "id": "rEoEKuOAHKFsQMYM04S_G",
-            "recordedDates": [
-                "Sat Apr 03 2021",
-            ]
-        };
-        const outputStreaks = getStreaks(testObject.recordedDates);
-        const expectedOutput = [
-            {
-                dates: testObject.recordedDates.map(dateString => Date.parse(dateString)),
-            }
-        ];
-        expect(outputStreaks).toEqual(expectedOutput);
-    });
+  it('returns single streak that is current', () => {
+    const testObject = {
+      name: 'Develop Habbit app',
+      id: 'rEoEKuOAHKFsQMYM04S_G',
+      recordedDates: [new Date().toDateString()],
+    };
+    const outputStreaks = getStreaks(testObject.recordedDates);
+    const expectedOutput = [
+      {
+        dates: testObject.recordedDates.map(dateString =>
+          Date.parse(dateString),
+        ),
+        isCurrentStreak: true,
+      },
+    ];
+    expect(outputStreaks).toEqual(expectedOutput);
+  });
 
-    it('returns three streaks streak with entries', () => {
-        const testObject = {
-            "name": "Develop Habbit app",
-            "id": "rEoEKuOAHKFsQMYM04S_G",
-            "recordedDates": [
-                "Sat Apr 22 2021",
-                "Fri Apr 21 2021",
-                "Sat Apr 20 2021",
-                "Sat Apr 03 2021",
-                "Fri Apr 02 2021",
-                "Sat Mar 1 2021",
-            ]
-        };
-        const outputStreaks = getStreaks(testObject.recordedDates);
-        const expectedOutput = [
-            {
-                dates: [
-                    Date.parse("Sat Apr 22 2021"),
-                    Date.parse("Fri Apr 21 2021"),
-                    Date.parse("Sat Apr 20 2021"),
-                ]
-            },
-            {
-                dates: [
-                    Date.parse("Sat Apr 03 2021"),
-                    Date.parse("Fri Apr 02 2021"),
-                ]
-            },
-            {
-                dates: [Date.parse("Sat Mar 1 2021")]
-            },
-
-        ]
-        expect(outputStreaks).toEqual(expectedOutput);
-    });
-
-
-
+  it('returns three streaks streak with entries', () => {
+    const testObject = {
+      name: 'Develop Habbit app',
+      id: 'rEoEKuOAHKFsQMYM04S_G',
+      recordedDates: [
+        'Sat Apr 22 2021',
+        'Fri Apr 21 2021',
+        'Sat Apr 20 2021',
+        'Sat Apr 03 2021',
+        'Fri Apr 02 2021',
+        'Sat Mar 1 2021',
+      ],
+    };
+    const outputStreaks = getStreaks(testObject.recordedDates);
+    const expectedOutput = [
+      {
+        dates: [
+          Date.parse('Sat Apr 22 2021'),
+          Date.parse('Fri Apr 21 2021'),
+          Date.parse('Sat Apr 20 2021'),
+        ],
+      },
+      {
+        dates: [Date.parse('Sat Apr 03 2021'), Date.parse('Fri Apr 02 2021')],
+      },
+      {
+        dates: [Date.parse('Sat Mar 1 2021')],
+      },
+    ];
+    expect(outputStreaks).toEqual(expectedOutput);
+  });
 });
--- src/utils/calculateStreaks.ts
+++ src/utils/calculateStreaks.ts
@@ -1,34 +1,37 @@
-import { differenceInCalendarDays, isToday, isYesterday } from "date-fns";
+import {differenceInCalendarDays, isToday, isYesterday} from 'date-fns';
 
-export const getStreaks = (streaksArray: string[]): { dates: number[], isCurrentStreak?: boolean }[] => {
-    let streaks: { dates: number[], isCurrentStreak?: boolean }[] = [];
+export const getStreaks = (
+  streaksArray: string[],
+): {dates: number[]; isCurrentStreak?: boolean}[] => {
+  let streaks: {dates: number[]; isCurrentStreak?: boolean}[] = [];
 
-    const streaksAsDates = streaksArray.map(stringDate => Date.parse(stringDate));
-    if (streaksAsDates.length === 1) {
-        streaks.push({dates: streaksAsDates});
-        return streaks;
-    }
-
+  const streaksAsDates = streaksArray.map(stringDate => Date.parse(stringDate));
+  if (streaksAsDates.length === 1) {
+    streaks.push({dates: streaksAsDates});
+  } else {
     streaksAsDates.forEach((date, index) => {
-        const previousDate = streaksAsDates[index - 1];
-        const currentStreak = streaks[streaks.length -1];
-        const isCurrentStreak = isToday(date) || isYesterday(date);
+      const previousDate = streaksAsDates[index - 1];
+      const currentStreak = streaks[streaks.length - 1];
 
-        if (!previousDate) {
-            streaks.push({ dates: [date], });
+      if (!previousDate) {
+        streaks.push({dates: [date]});
+      } else {
+        const daysSinceLastDate = differenceInCalendarDays(previousDate, date);
+        if (daysSinceLastDate === 1 || daysSinceLastDate === -1) {
+          currentStreak.dates.push(date);
         } else {
-            const daysSinceLastDate = differenceInCalendarDays(previousDate, date);
-            if (daysSinceLastDate === 1 || daysSinceLastDate === -1) {
-                currentStreak.dates.push(date);
-            } else {
-                streaks.push({ dates: [date] });
-            }
+          streaks.push({dates: [date]});
         }
-
-        if (isCurrentStreak) {
-            streaks[streaks.length -1].isCurrentStreak = true;
-        }
+      }
     });
+  }
 
-    return streaks;
-}
+  if (streaks[0]) {
+    const latestDate = streaks[0].dates[0];
+    if (isToday(latestDate) || isYesterday(latestDate)) {
+      streaks[0].isCurrentStreak = true;
+    }
+  }
+
+  return streaks;
+};
