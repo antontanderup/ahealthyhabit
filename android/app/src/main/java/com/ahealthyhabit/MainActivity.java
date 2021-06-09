1657e726a5cad94c5f21409f9d86162b1aba4fcf
Anton Tanderup Adamsen <antontandrup@gmail.com>
2021-06-09 10:44:31 +0200

    add react-native-gesture-handler

--- android/app/src/main/java/com/ahealthyhabit/MainActivity.java
+++ android/app/src/main/java/com/ahealthyhabit/MainActivity.java
@@ -2,6 +2,11 @@ package com.ahealthyhabit;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
+
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
@@ -13,6 +18,16 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "AHealthyHabit";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 
   @Override
